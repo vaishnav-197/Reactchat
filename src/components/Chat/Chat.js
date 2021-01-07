092ee64e1b45f88aacfd50e16f079f4ad920713b
vaishnav-197 <vaishnavea18@gmail.com>
2021-01-07 17:12:44 +0530

    changes endpoint

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -17,7 +17,7 @@ const Chat = ({ location }) => {
   const [users, setUsers] = useState('');
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
-  const ENDPOINT = 'http://localhost:5000/';
+  const ENDPOINT = 'https://reactchatserver.herokuapp.com/';
 
   useEffect(() => {
     const { name, room } = queryString.parse(location.search);
